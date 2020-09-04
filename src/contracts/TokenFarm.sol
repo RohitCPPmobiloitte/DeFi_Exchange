b94c802b83d5e7e616fe896870a0b9493e9c684d
MXmaster2s <bullzeye711@gmail.com>
2020-09-04 21:13:06 +0530

    adding unstake token functionality

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -36,12 +36,29 @@ contract TokenFarm {
         //update staking status
         hasStaked[msg.sender] = true;
         isStaking[msg.sender] = true;
-
         //issue tokens to the caller
         issueTokens(msg.sender);
     }
 
     //unstake tokens (withdraw)
+    function unstakeTokens(uint _amount) public {
+        //retrieve token balance of user 
+        uint balance = stakingBalance[msg.sender];      
+        //user balance should be greater or equal to unstake amount
+        require(balance >= _amount, "Cannot unstake more tokens than you have staked!");
+        //updated balance to be staked
+        uint updatedBalance = balance - _amount;
+        //updating staking balance
+        stakingBalance[msg.sender] -= updatedBalance;
+        //transferring dappTokens from user to tokenFarm    
+        dappToken.transferFrom(msg.sender, address(this), _amount);
+        //transferring mDAI tokens from tokenFarm to user
+        daiToken.transferFrom(address(this), msg.sender, _amount);
+        //if updated token balance of user is zero then make isStaking false 
+        if(updatedBalance <= 0){
+            isStaking[msg.sender] = false;
+        }
+    }
 
     //issue tokens
     function issueTokens(address recipient) public {
