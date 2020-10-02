248a9e88424ec14ce98e471d462c5eda557e96f9
RohitCPPmobiloitte <cpp-rohit@mobiloitte.com>
2020-10-02 17:00:50 +0530

    Updating migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,20 @@
-const TokenFarm = artifacts.require("TokenFarm");
-const DappToken = artifacts.require("DappToken");
+const Vault = artifacts.require("Vault");
 const DaiToken = artifacts.require("DaiToken");
+const WBTCToken = artifacts.require("WBTCToken");
 
 module.exports = async function(deployer, network, accounts) {
     
-  await deployer.deploy(DappToken); //deploy dappToken
-  const dappToken = await DappToken.deployed();
-  
-  await deployer.deploy(DaiToken);  //deploy daiToken
+  await deployer.deploy(DaiToken); //deploy DaiToken
   const daiToken = await DaiToken.deployed();
   
-  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address); //deploy tokenFarm
-  const tokenFarm = await TokenFarm.deployed();
+  await deployer.deploy(WBTCToken);  //deploy wbtcToken
+  const wbtcToken = await WBTCToken.deployed();
+  
+  await deployer.deploy(Vault, daiToken.address, wbtcToken.address); //deploy vault
+  const vault = await Vault.deployed();
 
-  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000'); //transfer all 1mn dapptokens to account[0] == tokenFarm address
+  await daiToken.transfer(vault.address, '1000000000000000000000000'); //transfer all 1mn DaiToken to vault address
 
-  await daiToken.transfer(accounts[1], '100000000000000000000')  //transfer all 100 daiTokens to account[1] == investor
+  await wbtcToken.transfer(accounts[1], '100000000000000000000')  //transfer all 100 wbtcTokens to account[1] == investor
 
 };
