da723cf9da37c4cbf0432873b797f498432cb2b5
RohitCPPmobiloitte <cpp-rohit@mobiloitte.com>
2020-10-02 17:40:32 +0530

    tweaking frontend

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import farmer from '../farmer.png'
 
 class Navbar extends Component {
 
@@ -8,12 +7,11 @@ class Navbar extends Component {
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src={farmer} width="30" height="30" className="d-inline-block align-top" alt="" />
-          &nbsp; DApp Token Farm
+          &nbsp; wBTC - DAI DeFi Portal
         </a>
 
         <ul className="navbar-nav px-3">
